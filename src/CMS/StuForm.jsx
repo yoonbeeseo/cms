7f144039fb813552c119ffc38513e293dd7f46b4
uidata11 <chan7pp@naver.com>
2025-02-14 10:46:42 +0900

    init

---
+++ src/CMS/StuForm.jsx
@@ -0,0 +1,17 @@
+const StuForm = () => {
+  return (
+    <div>
+      <h1>StuForm</h1>
+      <div>
+        App
+        <form action="">
+          <input type="text" />
+          <button>수정</button>
+          <button>삭제</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default StuForm;
