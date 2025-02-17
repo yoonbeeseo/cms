00386a3d9582c95cf150f8ce1a000b38a943b66f
wonbinwin <youngkf@naver.com>
2025-02-17 12:15:28 +0900

    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
 import StuForm from "./CMS/StuForm.jsx";
 import StuItem from "./CMS/StuItem";
+import { useState } from "react";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+
   return (
     <div>
-      <StuForm />
+      <ul>
+        {users.map((user, index) => {
+          return (
+            <StuItem
+              key={user.studentid}
+              index={index}
+              user={user}
+              users={users}
+              setUsers={setUsers}
+            />
+          );
+        })}
+      </ul>
+      <StuForm setUsers={setUsers} users={users} />
       <StuItem />
     </div>
   );
