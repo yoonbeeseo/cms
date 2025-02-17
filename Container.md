32f37b91615e3bf9007775a319e01c5092ade15b
uidata11 <chan7pp@naver.com>
2025-02-17 12:08:49 +0900

    1

--- Container.md
+++ Container.md
@@ -1,11 +1,26 @@
 # 변수명
 
-users,
+App.jsx
+users 배열
 
 # item
 
+받아올 값
+
 itemUser 변수명으로 받아오기 (오브젝트로)
+
+index,
+itemUsers(배열, 중복검사) ,
 
 # form
 
-payload, setUsers, users, isEditing, onCancel
+받아올 값
+
+users(배열, 중복검사),
+
+setUsers 함수(추가, 수정),
+
+payload 오브젝트(수정용),
+
+isEditing(불리언타입, 수정),
+onCancel 함수(취소)
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import StuForm from "./CMS/StuForm.jsx";
 import StuItem from "./CMS/StuItem";
+import { studentin } from "./database.js";
 
 const App = () => {
+  const [users, setUsers] = useState([studentin]);
+
   return (
     <div>
       <StuForm />
-      <StuItem />
+      <StuItem user={studentin} />
     </div>
   );
 };
