30ed43d1e84d485b3d1335d21c1b6b101b6ebe19
wonbinwin <youngkf@naver.com>
2025-02-19 12:22:54 +0900

    0219

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,41 @@ import "./App.css";
 
 const App = () => {
   const [users, setUsers] = useState([studentin]);
-  const [users, setUsers] = useState([studentin]);
 
   return (
-    <div>
-      <StuForm setUsers={setUsers} users={users} />
-      <ul>
-        {users.map((user, index) => {
-          return (
-            <StuItem
-              key={user.studentid}
-              index={index}
-              user={user}
-              users={users}
-              setUsers={setUsers}
-            />
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <div className="title">
+        <h1>CMS</h1>
+      </div>
+      <div>
+        <StuForm setUsers={setUsers} users={users} />
+        <ul>
+          {users.map((user, index) => {
+            return (
+              <StuItem
+                key={user.studentid}
+                index={index}
+                user={user}
+                users={users}
+                setUsers={setUsers}
+              />
+              <div>img</div>
+          <div>학생{index + 1}</div>
+          <div>
+            {user.name}
+            {user.gender}
+            {user.birth}
+            {user.address}
+            {user.tel}
+            <p>ID: 00000{index + 1}</p>
+          </div>
+          <button onClick={editUser}>수정</button>
+          <button onClick={onDelete}>삭제</button>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
