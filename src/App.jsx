29eb88c3f9e7a581e04c73290b0e86fbb267e8be
uidata11 <chan7pp@naver.com>
2025-02-21 09:45:48 +0900

    master

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,20 @@ import "./App.css";
 
 const App = () => {
   // localStorage에서 'users' 데이터를 불러옵니다. 없으면 빈 배열을 기본값으로 사용합니다.
-  const storedUser = JSON.parse(localStorage.getItem("users"));
-  const [users, setUsers] = useState(storedUser);
+  // const storedUser = JSON.parse(localStorage.getItem("users"));
+
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     localStorage.setItem("users", JSON.stringify(users));
     console.log("updated");
   });
+
+  useEffect(() => {
+    const user = localStorage.getItem("users");
+    if (user) {
+      setUsers(JSON.parse(user));
+    }
+  }, []);
 
   return (
     <>
