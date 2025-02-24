2cbbaf70d0bbfcba85f181a46f4dba8b91ba4445
yoonbeeseo <yoon.tec.info@gmail.com>
2025-02-24 09:28:51 +0900

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import StuForm from "./CMS/StuForm.jsx";
-import StuItem from "./CMS/StuItem";
-import { useState, useEffect } from "react";
-import { studentin } from "./database.js";
-import "./App.css";
+import StuForm from "./CMS/StuForm.jsx"
+import StuItem from "./CMS/StuItem"
+import { useState, useEffect } from "react"
+import "./App.css"
 
 const App = () => {
   // localStorage에서 'users' 데이터를 불러옵니다. 없으면 빈 배열을 기본값으로 사용합니다.
   // const storedUser = JSON.parse(localStorage.getItem("users"));
 
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    localStorage.setItem("users", JSON.stringify(users));
-    console.log("updated");
-  });
+  const [users, setUsers] = useState(JSON.parse(localStorage.getItem("users")) ?? [])
+  //! 최초렌더링 시 빈 배열을 부여하면 아래의 아무런 배열이 없는 useEffect에서 users를 빈 배열로 만들어버리는 버그 발생
+  //! 초기값을 로컬스토레지 안의 users를 검사하여 주는 방식으로 변경
 
   useEffect(() => {
-    const user = localStorage.getItem("users");
+    localStorage.setItem("users", JSON.stringify(users))
+  })
+
+  useEffect(() => {
+    const user = localStorage.getItem("users")
     if (user) {
-      setUsers(JSON.parse(user));
+      setUsers(JSON.parse(user))
     }
-  }, []);
+  }, [])
 
   return (
     <>
@@ -30,19 +31,11 @@ const App = () => {
         <StuForm setUsers={setUsers} users={users} />
         <ul className="stu">
           {users.map((user, index) => {
-            return (
-              <StuItem
-                key={user.studentid}
-                index={index}
-                user={user}
-                users={users}
-                setUsers={setUsers}
-              />
-            );
+            return <StuItem key={user.studentid} index={index} user={user} users={users} setUsers={setUsers} />
           })}
         </ul>
       </div>
     </>
-  );
-};
-export default App;
+  )
+}
+export default App
